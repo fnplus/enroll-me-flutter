2ae86ac3423ff627fe17526468038c2755f88a25
pratik037 <singhalpratik037@gmail.com>
2019-10-28 15:10:53 +0530

    Made UI responsive. Added TODOs yet to be done.

--- lib/app/widgets/customCard.dart
+++ lib/app/widgets/customCard.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 class CustomCard extends StatelessWidget {
   final double height;
   final double width;
   final String text;
   final int code;
   final bool isDark;
+  final String imageName;
+  final String routeName;
 
   const CustomCard(
       {Key key,
@@ -12,25 +16,46 @@ class CustomCard extends StatelessWidget {
       @required this.width,
       @required this.text,
       @required this.code,
-      @required this.isDark})
+      @required this.isDark,
+      @required this.imageName,
+      this.routeName})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Material(
-        child: Container(
-          height: height,
-          width: width,
-          alignment: Alignment.center,
-          child: isDark
-              ? Text(
-                  text,
-                  style: TextStyle(color: Colors.white),
-                )
-              : Text(text),
-          decoration: BoxDecoration(
-              color: Color(code), borderRadius: BorderRadius.circular(20)),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(routeName);
+        },
+        child: Material(
+          elevation: 3,
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            height: height,
+            width: width,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SvgPicture.asset('assets/images/' + imageName,
+                    height: height * 0.80),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: isDark
+                      ? Text(
+                          text,
+                          style: TextStyle(color: Colors.white),
+                        )
+                      : Text(text),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Color(code),
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
         ),
       ),
     );
