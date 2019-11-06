bdc4ec60dd7efd8a57142936e0b8541e7ebe9604
Uniyal <abhishekuniyal09@gmail.com>
2019-11-06 13:04:34 +0530

    Add licence file

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/pratik037/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/pratik037/Flutter Projects/project-enroll-me-flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/abhishek/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abhishek/FlutterProjects/enroll-me-flutter"
+export "FLUTTER_TARGET=/Users/abhishek/FlutterProjects/enroll-me-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/pratik037/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/abhishek/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
