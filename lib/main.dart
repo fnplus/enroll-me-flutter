6cc0cc8c70d3a0a4c63eae9158869b5566d22ea5
Youness Dominique <youness.dominique@gmail.com>
2022-11-25 17:10:36 +0200

    [FIX] Android configuration and upgrade information

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:enroll_me/app/enrollme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  initializeDefault();
   runApp(EnrollMe());
+}
+
+Future<void> initializeDefault() async {
+  FirebaseApp app = await Firebase.initializeApp();
 }
